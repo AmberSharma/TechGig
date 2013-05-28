b433f78ea3a0310e70ebc5f0258dd4e81e3d5ed7
AmberSharma <amber.sharma@osscube.com>
2013-05-28 06:42:42 -0700

    house_condition

--- homecondition.php
+++ homecondition.php
@@ -1,3 +1,45 @@
+<!--
+Visit a Colony
+Problem Statement
+In a colony all houses are in a single line. We call a house either in a good condition or in a bad condition. If a house is good its score is 1 otherwise 0. We decided to perform the scoring of houses in a different way. 
+We not only consider that particular house but also the 2 neighboring house.
+
+New scoring strategy- The Score of a house if affected by three house, that house and its two neighbors
+
+Score-1 : If any of the three houses is in good condition 
+Score-2 : if any of the two houses is in good condition 
+Score-3 : if all the three houses are in good condition 
+
+Now Josheph has the score list of all the houses ( according to new strategy ) and he wants to know what is the condition ( good or bad) of his house just seeing the score of the houses. He assumes that first house is in good condition.
+
+Contest ends on Thu May 30, 2013 at 23:59:00
+
+
+Input/Output Specifications
+Input Specification:
+
+(1) An integer array of score of all the houses in order 
+(2) House number of Josheph 
+
+Output Specification: 
+
+If house condition of Josheph is good then your output will be 1.
+If house condition of Josheph is bad then your output will be 0.
+If assumption about house1 doesnt match the score array then your output will be -1.
+
+
+Examples
+Let say the score list is as follows 
+
+{2,2,1,1,1} 
+
+Now if Josheph's house number is 4 
+His Condition is bad (0).
+
+
+
+ 
+-->
 <?php
    function house_condition($input1,$input2)
 	{
